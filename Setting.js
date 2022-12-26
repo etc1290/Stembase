3d730a231f0bd36ce0eb243fdb49df352174a918
Paiiimannnoitla <121204598+Paiiimannnoitla@users.noreply.github.com>
2022-12-26 11:17:58 +0800

    Infrastructure building
    Project folder re-arrange and setting file implement

---
+++ Setting.js
@@ -0,0 +1,8 @@
+//Basic system setting down below
+module.exports = {
+	TemplateDir:	'./template/',
+	StaticDir:		'./static/',
+	Debugmode:		true,
+}
+
+
--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const fs = require('fs')
 
+// Import System Variable here
+var env=require('./Setting.js')
+
+console.log(env.TemplateDir)
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
@@ -13,7 +17,7 @@ const createWindow = () => {
     win.webContents.openDevTools()
     const files = fs.readdirSync('./');
     ipcMain.handle('main', () => files)
-    win.loadFile('index.html')
+    win.loadFile(env.TemplateDir + 'index.html')
 }
 
 
