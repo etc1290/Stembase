2b18fb51af5c8cec01f13884c16d2fb19895edcd
Paiiimannnoitla <121204598+Paiiimannnoitla@users.noreply.github.com>
2022-12-27 14:48:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ const fs = require('fs')
 // Import System Variable here
 var env=require('./Setting.js')
 
+<<<<<<< Updated upstream
 console.log(env.TemplateDir)
+=======
+
+// WindowsCreator
+//--- Window create function
+
+>>>>>>> Stashed changes
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
@@ -16,11 +23,35 @@ const createWindow = () => {
     })
     win.webContents.openDevTools()
     const files = fs.readdirSync('./');
+<<<<<<< Updated upstream
     ipcMain.handle('main', () => files)
     win.loadFile(env.TemplateDir + 'index.html')
 }
 
 
+=======
+    ipcMain.handle('fs-main',	() => files)
+	
+// DarkMode
+	// Main: toggle
+	ipcMain.handle('dm-main',	() =>{
+		console.log(nativeTheme.shouldUseDarkColors)
+		if (nativeTheme.shouldUseDarkColors){
+			nativeTheme.themeSource = 'light'
+			
+		}else{
+			nativeTheme.themeSource = 'dark'
+			
+		}
+		return nativeTheme.shouldUseDarkColors
+	})
+	// Side: Reset to system
+	ipcMain.handle('dm-reset',	() =>{
+		nativeTheme.themeSource = 'system'
+	})
+}
+    
+>>>>>>> Stashed changes
 app.whenReady().then(() => {
     createWindow()
 })
