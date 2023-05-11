347e137d616695f0e72bb22a883b7e6a4aac1759
Paiiimannnoitla <121204598+Paiiimannnoitla@users.noreply.github.com>
2023-05-11 14:07:19 +0800

    Import function to local
    Import mntdrag to index-mnt.js

--- template/index-mnt.js
+++ template/index-mnt.js
@@ -1,3 +1,78 @@
+// Side: drag remake
+const mntdragSetup = function() {
+  var mntdrag,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments) } }
+
+  mntdrag = (function() {
+    function mntdrag(el) {
+      this.el = el
+      this.dragleave = __bind(this.dragleave, this)
+      this.dragenter = __bind(this.dragenter, this)
+      if (this.supportsEventConstructors()) {
+        this.first = false
+        this.second = false
+        this.el.addEventListener("dragenter", this.dragenter, false)
+        this.el.addEventListener("dragleave", this.dragleave, false)
+      }
+    }
+
+    mntdrag.prototype.dragenter = function(event) {
+      if (this.first) {
+        return this.second = true
+      } else {
+        this.first = true
+        return this.el.dispatchEvent(new CustomEvent("mntdrag:enter", {
+          bubbles: true,
+          cancelable: true,
+          detail: {
+            dataTransfer: event.dataTransfer
+          }
+        }))
+      }
+    }
+
+    mntdrag.prototype.dragleave = function(event) {
+      if (this.second) {
+        this.second = false
+      } else if (this.first) {
+        this.first = false
+      }
+      if (!this.first && !this.second) {
+        return this.el.dispatchEvent(new CustomEvent("mntdrag:leave", {
+          bubbles: true,
+          cancelable: true,
+          detail: {
+            dataTransfer: event.dataTransfer
+          }
+        }))
+      }
+    }
+
+    mntdrag.prototype.removeListeners = function() {
+      this.el.removeEventListener("dragenter", this.dragenter, false)
+      return this.el.removeEventListener("dragleave", this.dragleave, false)
+    }
+
+    mntdrag.prototype.supportsEventConstructors = function() {
+      try {
+        new CustomEvent("z")
+      } catch (_error) {
+        return false
+      }
+      return true
+    }
+
+    mntdrag.prototype.reset = function() {
+      this.first = false
+      return this.second = false
+    }
+
+    return mntdrag
+
+  })()
+  window.mntdrag = mntdrag
+}
+mntdragSetup()
 // Side:	Target class checker
 const mntcheck = (event,classname,isCurrent=false)=>{
 	if(isCurrent){
